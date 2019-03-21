4b24dd192c22a3da566f09e5ad7a03388d940299
Janxxy <lesley.pond@gmail.com>
2019-03-20 22:33:34 -0400

    fixed totalling

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -46,58 +46,60 @@ $("#friesAdd").on("click", function(event){
 
 
 function addWingsToCart() {
-  let amount;
+  
+  let amount = $("#wingsQuantity").val()
   let price;
+  let quantity;
   if ($("#wingType").val() && $("#wingsHowManySelect").val() && $("#wingsFlavorSelect").val() && $("#wingsQuantity").val()) {
-
-    amount = $("#wingsHowManySelect").find(':selected').data('amount')
     price = $("#wingsHowManySelect").find(':selected').data('price')
+    quantity = $("#wingsHowManySelect").find(':selected').data('amount')
   } else if ($("#wingType").val() && $("#bonelessHowManySelect").val() && $("#wingsFlavorSelect").val() && $("#wingsQuantity").val()) {
-
-    amount = $("#bonelessHowManySelect").find(':selected').data('amount')
     price = $("#bonelessHowManySelect").find(':selected').data('price')
+    quantity = $("#bonelessHowManySelect").find(':selected').data('amount')
   } else if ($("#wingType").val() && $("#veganHowManySelect").val() && $("#wingsFlavorSelect").val() && $("#wingsQuantity").val()) {
-    amount = $("#veganHowManySelect").find(':selected').data('amount')
     price = $("#veganHowManySelect").find(':selected').data('price')
+    quantity = $("#veganHowManySelect").find(':selected').data('amount')
   }
   else{
     return $(".selectFields").show();
   }
+  
   $(".addedToCart").show()
   let newRow = $("<tr>")
-  console.log(price)
-  let priceCell = $("<td>" + price + "</td>")
+  let priceCell = $("<td>" + (price * amount) + "</td>")
   priceCell.attr("class", "itemPrice")
-  newRow.append("<td>" + $("#wingType").val() + " (" + $("#wingsFlavorSelect").val() + ")</td>")
+  newRow.append("<td>" + $("#wingType").val() + " (" + $("#wingsFlavorSelect").val() + ")(" +quantity+ ")</td>")
   let quantityCell=$("<td>"+amount+"</td>")
-  quantityCell.attr("class","quantitySub")
   newRow.append(quantityCell);
   newRow.append(priceCell)
   
 
   $("#cartTable").append(newRow)
   let sum = 0;
-  $(".itemPrice").each(function () {
+  $(".itemPrice").each(function(){
     var value = $(this).text();
     if (!isNaN(value) && value.length != 0) {
-      sum += parseFloat(value)
+      sum += Math.round(value * 100) / 100
     }
+    sum =  Math.round(sum * 100) / 100
   })
 
+  
   $("#cartTotal").text("$" + sum)
   $("#wingsForm").trigger("reset");
 
 }
-let priceID=0;
+
+
+
+
 function addFriesToCart(){
 let amount;
 let price;
-let quantity;
-priceID ++;
 if($("#friesType").val() && $("#friesHowManySelect").val() && $("#friesQuantity").val()){
 amount=$("#friesQuantity").val()
 price= $("#friesHowManySelect").find(":selected").data("price");
-quantity=$("#friesQuantity").val();
+
 }
 else{
   return $(".selectFields").show();
@@ -106,37 +108,24 @@ else{
 $(".addedToCart").show()
 let newRow = $("<tr>")
 console.log(price)
-let priceCell = $("<td>" + price + "</td>")
-priceCell.attr("data-number", priceID)
+let priceCell = $("<td>" + (price * amount) + "</td>")
 priceCell.attr("class", "itemPrice")
 newRow.append("<td>" + $("#friesType").val() + "("+$("#friesHowManySelect").find(":selected").data("amount") +")</td>")
 let quantityCell = $("<td>" + amount + "</td>")
-quantityCell.attr("data-number", priceID)
-quantityCell.attr("class", "itemQuantity")
 newRow.append(quantityCell)
 newRow.append(priceCell)
-
-
 $("#cartTable").append(newRow)
 let sum = 0;
-let quantity2;
-let tag;
-$(".itemPrice").each(function () {
-  tag = $(this).attr("data-number");
-  console.log(tag)
-       $(".itemQuantity").each(function(){
-         if(tag = $(this).attr("data-number")){
-          quantity2 = $(this).text();
-          console.log(quantity2)
-         }
-       })
+  $(".itemPrice").each(function(){
+    var value = $(this).text();
+    if (!isNaN(value) && value.length != 0) {
+      sum += Math.round(value * 100) / 100
+    }
+    sum =  Math.round(sum * 100) / 100
+  })
+ 
   
-  var value = $(this).text();
-  if (!isNaN(value) && value.length != 0) {
-    sum += (parseFloat(value) * parseInt(quantity2))
-    
-  }
-})
+
 
 $("#cartTotal").text("$" + sum)
 $("#friesForm").trigger("reset");
